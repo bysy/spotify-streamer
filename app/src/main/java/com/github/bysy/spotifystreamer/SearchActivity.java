8d4cc044928d4d22f2d3c223acbea1d686baadfe
Benjamin <bysy@users.noreply.github.com>
2015-06-14 01:47:25 -0500

    initial commit

---
+++ app/src/main/java/com/github/bysy/spotifystreamer/SearchActivity.java
@@ -0,0 +1,117 @@
+package com.github.bysy.spotifystreamer;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
+public class SearchActivity extends AppCompatActivity {
+    private Artist[] mArtists;
+    private ArtistAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        // TODO: replace mock with live data
+        mArtists = mockArtists;
+        mAdapter = new ArtistAdapter(this, R.layout.single_artist, mArtists);
+
+        ListView lv = (ListView) findViewById(R.id.artistListView);
+        lv.setAdapter(mAdapter);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private final Artist[] mockArtists = makeMockArtists();
+    private Artist[] makeMockArtists() {
+        final int w = 256;
+        final int h = 256;
+        final Bitmap.Config c = Bitmap.Config.ARGB_8888;
+        final int[] colors = {0xff4dd0e1, 0xfffff176, 0xffffa726,
+                              0xff4db6ac, 0xfff06292, 0xff90a4ae};
+        final String[] names = {"Abc artist", "Def singer-songwriter", "Ghi super-group",
+                                "Jkl star", "Mno MC", "Pqr pop star"};
+        final int len = Math.min(colors.length, names.length);
+        Artist[] artists = new Artist[len];
+        for (int i = 0; i<len; ++i) {
+            Bitmap b = Bitmap.createBitmap(w, h, c);
+            b.eraseColor(colors[i]);
+            artists[i] = new Artist(names[i], b);
+        }
+        return artists;
+    }
+
+    class ArtistAdapter extends ArrayAdapter<Artist> {
+        private Artist[] mArtists;
+        private final int mResource;
+
+        ArtistAdapter(Context context, int resource, Artist[] artists) {
+            super(context, resource, artists);
+            mResource = resource;
+            mArtists = artists;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View item = convertView;
+            if (item==null) {
+                LayoutInflater li = getLayoutInflater();
+                item = li.inflate(mResource, parent, false);
+            }
+            Artist artist = getItem(position);
+            TextView tv = (TextView) item.findViewById(R.id.artistNameView);
+            tv.setText(artist.getName());
+            ImageView iv = (ImageView) item.findViewById(R.id.artistImageView);
+            iv.setImageBitmap(artist.getImage());
+            return item;
+        }
+
+    }
+}
+
+class Artist {
+    private final String name;
+    private final Bitmap image;
+
+    public Artist(String name, Bitmap image) {
+        this.name = name;
+        this.image = image;
+    }
+
+    String getName() { return name; }
+    Bitmap getImage() { return image; }
+
+}
