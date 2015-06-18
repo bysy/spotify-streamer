f2925eb45463836af5917ed4beaeab87d97a036b
Benjamin <bysy@users.noreply.github.com>
2015-06-17 21:05:40 -0500

    feat: Populate top songs ListView

--- app/src/main/java/com/github/bysy/spotifystreamer/TopSongsActivity.java
+++ app/src/main/java/com/github/bysy/spotifystreamer/TopSongsActivity.java
@@ -1,11 +1,18 @@
 package com.github.bysy.spotifystreamer;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 
 public class TopSongsActivity extends AppCompatActivity {
@@ -25,6 +32,19 @@ public class TopSongsActivity extends AppCompatActivity {
             return;
         }
         ab.setTitle(name);
+        Song[] songs = sMockSongs;
+        SongsAdapter adapter = new SongsAdapter(this, R.layout.song_list_item, songs);
+        ListView lv = (ListView) findViewById(R.id.songsListView);
+        lv.setAdapter(adapter);
+    }
+
+    static private final Song[] sMockSongs = makeMockSongs();
+    private static Song[] makeMockSongs() {
+        return new Song[]{new Song("Great Song", "Sweet Album"),
+                          new Song("Splendiferous Song", "Sweet Album"),
+                          new Song("OK Song", "Second Album"),
+                          new Song("More of the Same", "Third Album"),
+                          new Song("'Tis What We Do", "Sweet Album")};
     }
 
 
@@ -48,5 +68,40 @@ public class TopSongsActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    class SongsAdapter extends ArrayAdapter<Song> {
+        private final int mResource;
+
+        SongsAdapter(Context context, int resource, Song[] songs) {
+            super(context, resource, songs);
+            mResource = resource;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View item = convertView;
+            if (item==null) {
+                LayoutInflater li = getLayoutInflater();
+                item = li.inflate(mResource, parent, false);
+            }
+            Song song = getItem(position);
+            TextView tv = (TextView) item.findViewById(R.id.songNameView);
+            tv.setText(song.name);
+            tv = (TextView) item.findViewById(R.id.albumNameView);
+            tv.setText(song.album);
+            return item;
+        }
+    }
+}
+
+
+class Song {
+    public final String name;
+    public final String album;
+
+    Song (String name, String album) {
+        this.name = name;
+        this.album = album;
     }
 }
