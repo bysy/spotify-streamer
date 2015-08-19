ad4e2e18bbb9e1d322d87dfd7b02edf7d940956d
Benjamin <bysy@users.noreply.github.com>
2015-08-18 23:54:50 -0500

    feat: Stream song preview

--- app/src/main/java/com/github/bysy/spotifystreamer/PlayerActivityFragment.java
+++ app/src/main/java/com/github/bysy/spotifystreamer/PlayerActivityFragment.java
@@ -5,19 +5,23 @@
 package com.github.bysy.spotifystreamer;
 
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
 
 /**
  * Play a song.
  */
-public class PlayerActivityFragment extends Fragment {
+public class PlayerActivityFragment extends Fragment implements MediaPlayer.OnPreparedListener, MediaPlayer.OnCompletionListener {
 
     private static final String TAG = PlayerActivityFragment.class.getSimpleName();
     private ImageView mAlbumImageView;
@@ -47,5 +51,36 @@ public class PlayerActivityFragment extends Fragment {
         Intent in = getActivity().getIntent();
         String imageUrl = in.getStringExtra(TopSongsActivityFragment.Key.ALBUM_IMAGE_URL);
         Util.loadImageInto(getActivity(), imageUrl, mAlbumImageView);
+
+        // Don't start another player when the orientation etc changes.
+        if (savedInstanceState!=null) {
+            return;
+        }
+        String previewUrl = in.getStringExtra(TopSongsActivityFragment.Key.SONG_PREVIEW_URL);
+        if (previewUrl==null) {
+            return;
+        }
+        MediaPlayer player = new MediaPlayer();
+        player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        Log.d(TAG, "Trying to play ".concat(previewUrl));
+        try {
+            player.setDataSource(previewUrl);
+        } catch (IOException e) {
+            Log.e(TAG, "Setting of preview url failed.");
+            e.printStackTrace();
+        }
+        player.setOnPreparedListener(this);
+        player.setOnCompletionListener(this);
+        player.prepareAsync();
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        mp.start();
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        mp.release();
     }
 }
